74dc2682988d8243ed9c12f2e2b61d96676df28f
Victor Jr Garcia Montalvan <86797211+vgar98@users.noreply.github.com>
2022-01-21 13:57:21 -0500

    first

---
+++ datetoymdhi.js
@@ -0,0 +1,10 @@
+function retorna_fecha_ymdhi(fecha){
+    let new_fecha = new Date(fecha);
+    let anho = new_fecha.getFullYear();
+    let dia = new_fecha.getDate();
+    let mes = new_fecha.getMonth()+1;
+    let hora = new_fecha.getHours();
+    let min = new_fecha.getMinutes();
+    let fecha_nueva = `${anho}-${((mes+'').length<2?'0'+mes:mes)}-${((dia+'').length<2?'0'+dia:dia)}T${(hora+'').length<2?'0'+hora:hora}:${(min+'').length<2?'0'+min:min}`;
+    return fecha_nueva;
+}
